48d963a3961d9bdbe440e10899ea1fff4c9f4bca
Kolten Kerns <factionplus12@gmail.com>
2018-01-27 15:30:11 -0500

    Create PrintName.cs

---
+++ PrintName.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace ConsoleApp1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string myName;
+                Console.WriteLine("What is your name?");
+
+                myName = Console.ReadLine();
+
+                Console.WriteLine("Hello, {0}", myName);
+                Console.ReadLine();
+            }
+        }
+    }
